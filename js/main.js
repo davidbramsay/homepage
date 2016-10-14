21a36b8aa2e9d387799b48487d155b9bed53c007
David Ramsay <dramsay@mit.edu>
2016-10-14 00:05:11 -0400

    first commit

--- js/main.js
+++ js/main.js
@@ -5,6 +5,24 @@ $(document).ready(function() {
 
 	$(function () {
 		$('a[rel="lightbox"]').fluidbox();
-	})
+	});
 
+    $(window).on("load", function(){
+                collage();
+    });
+});
+
+function collage(){
+        $('.Collage').removeWhitespace().collagePlus({'fadeSpeed' : 2000, 'targetHeight' : 250,
+                                'direction': 'vertical'});
+};
+
+var resizeTimer = null;
+$(window).bind('resize', function() {
+    // hide all the images until we resize them
+    // set the element you are scaling i.e. the first child nodes of ```.Collage``` to opacity 0
+    $('.Collage .Image_Wrapper').css("opacity", 0);
+    // set a timer to re-apply the plugin
+    if (resizeTimer) clearTimeout(resizeTimer);
+    resizeTimer = setTimeout(collage, 200);
 });
